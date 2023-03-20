a83e8f460aa9c0e070635ef5e59e32f071461740
Jochen Jacobs <jochen.jacobs@campus.tu-berlin.de>
2023-03-20 03:22:55 +0100

    create icons script and workflow

---
+++ src/createItemIcons.js
@@ -0,0 +1,79 @@
+import { BlockDefinition, BlockModel, Identifier, ItemRenderer, TextureAtlas, upperPowerOfTwo } from "deepslate";
+import createContext from "gl";
+
+import '@loaders.gl/polyfills'
+import Jimp from "jimp";
+
+const MCMETA = 'https://raw.githubusercontent.com/misode/mcmeta/'
+
+const IMAGE_SIZE = 128
+
+const itemGl = createContext(IMAGE_SIZE, IMAGE_SIZE)
+const itemRenderer = await initalize(itemGl)
+
+async function initalize(itemGl) {
+    const [blockstates, models, uvMap] = await Promise.all([
+        fetch(`${MCMETA}summary/assets/block_definition/data.min.json`).then(r => r.json()),
+        fetch(`${MCMETA}summary/assets/model/data.min.json`).then(r => r.json()),
+        fetch(`${MCMETA}atlas/all/data.min.json`).then(r => r.json()),
+    ])
+    const blockDefinitions = {}
+    Object.keys(blockstates).forEach(id => {
+        blockDefinitions['minecraft:' + id] = BlockDefinition.fromJson(id, blockstates[id])
+    })
+
+    const blockModels = {}
+    Object.keys(models).forEach(id => {
+        blockModels['minecraft:' + id] = BlockModel.fromJson(id, models[id])
+    })
+    Object.values(blockModels).forEach((m) => m.flatten({ getBlockModel: (id) => blockModels[id] }))
+
+
+
+    const atlas = (await Jimp.read(`${MCMETA}atlas/all/atlas.png`))
+    
+    const atlasSize = upperPowerOfTwo(Math.max(atlas.bitmap.width, atlas.bitmap.height))
+    atlas.contain(atlasSize, atlasSize, Jimp.VERTICAL_ALIGN_TOP | Jimp.HORIZONTAL_ALIGN_LEFT)
+
+    const part = 16 / atlasSize
+    const idMap = {}
+    Object.keys(uvMap).forEach(id => {
+        const u = uvMap[id][0] / atlasSize
+        const v = uvMap[id][1] / atlasSize
+        idMap['minecraft:' + id] = [u, v, u + part, v + part]
+    })
+    const textureAtlas = new TextureAtlas(atlas.bitmap, idMap)
+
+    const resources = {
+        getBlockDefinition(id) { return blockDefinitions[id.toString()] },
+        getBlockModel(id) { return blockModels[id.toString()] },
+        getTextureUV(id) { return textureAtlas.getTextureUV(id) },
+        getTextureAtlas() { return textureAtlas.getTextureAtlas() },
+        getBlockFlags(id) { return { opaque: false } },
+        getBlockProperties(id) { return null },
+        getDefaultBlockProperties(id) { return null },
+    }
+
+    // === Item rendering ===
+
+
+    return new ItemRenderer(itemGl, Identifier.parse('stone'), resources)
+}
+
+export async function renderItem(item) {
+    itemGl.clearColor(0, 0, 0, 0)
+    itemGl.clear(itemGl.DEPTH_BUFFER_BIT | itemGl.COLOR_BUFFER_BIT)
+    itemRenderer.setItem(item)
+    itemRenderer.drawItem()
+
+    const pixels = new Uint8Array(IMAGE_SIZE * IMAGE_SIZE * 4)
+    itemGl.readPixels(0, 0, IMAGE_SIZE, IMAGE_SIZE, itemGl.RGBA, itemGl.UNSIGNED_BYTE, pixels)
+
+    new Jimp({data: pixels, width: IMAGE_SIZE, height: IMAGE_SIZE}).flip(false, true).write(`icons/item/${item.path}.png`)
+}
+
+const items = await fetch(`${MCMETA}registries/item/data.min.json`).then(r => r.json())
+
+for (const item of items){
+    renderItem(Identifier.create(item))
+}

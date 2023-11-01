f1eaa46265b3aa6c3125eb8c936e56270f7fbce5
jacobsjo <JochenJacobs33@gmail.com>
2023-11-01 20:09:31 +0100

    Setup OpenGL in action

--- src/createItemIcons.js
+++ src/createItemIcons.js
@@ -8,6 +8,7 @@ const MCMETA = 'https://raw.githubusercontent.com/misode/mcmeta/'
 const IMAGE_SIZE = 128
 
 const itemGl = createContext(IMAGE_SIZE, IMAGE_SIZE)
+console.log(itemGl)
 const itemRenderer = await initalize(itemGl)
 
 async function initalize(itemGl) {
